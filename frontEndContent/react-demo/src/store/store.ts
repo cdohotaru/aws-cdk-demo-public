109ca19334361132d667e6aeb2f732eb1384327b
Constantin Dohoraru <constantin.dohotaru@outlook.com>
2023-09-18 11:30:21 +0200

    Added logger and immutability check middlewares

--- frontEndContent/react-demo/src/store/store.ts
+++ frontEndContent/react-demo/src/store/store.ts
@@ -1,11 +1,15 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import { calculatorSlice } from '../features/calculator/calculatorSlice';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import { createLogger } from 'redux-logger';
 
 export const store = configureStore({
   reducer: {
     calculator: calculatorSlice.reducer,
   },
-});
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([createLogger(), reduxImmutableStateInvariant()])
+}, 
+);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
